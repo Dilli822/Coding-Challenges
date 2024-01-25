cfe2118776d6dc0c313082457c38d5e5b278e351
dilli <dillihangrae@gmail.com>
2024-01-25 06:54:04 +0545

    day 21

---
+++ ITSNP100DaysOfCoding/Day21/day21.js
@@ -0,0 +1,72 @@
+
+const html = `
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       // Add your code below this line
+
+
+
+       // Add your code above this line
+       .attr("x", 0)
+       .attr("y", 0)
+       .attr("width", 25)
+       .attr("height", 100);
+  </script>
+</body>
+`;
+
+const html2 = `
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h)
+                  // Add your code below this line
+
+
+
+                  // Add your code above this line
+  </script>
+</body>
+`;
+
+// Learn About SVG in D3
+const html3 = `
+<style>
+  svg {
+    background-color: pink;
+  }
+</style>
+<body>
+  <script>
+    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  // Add your code below this line
+
+
+
+                  // Add your code above this line
+  </script>
+</body>
+`
