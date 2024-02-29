5611513108e9e59479f69ae8fdc41756e46bfab9
dilli <dillihangrae@gmail.com>
2024-02-29 09:54:46 +0545

    day55classification for iris,keras,barh,splitarray

--- ITSNP100DaysOfCoding/Month2/Day54/day54.py
+++ ITSNP100DaysOfCoding/Month2/Day54/day54.py
@@ -7,6 +7,7 @@ import pandas as pd
 # Load data
 dftrain = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv") # training dataset
 dfeval = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/eval.csv") # testing data
+
 y_train = dftrain.pop("survived")
 y_eval = dfeval.pop("survived")
 
