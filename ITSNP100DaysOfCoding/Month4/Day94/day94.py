f2bd93ac769621fd8a1dc8c20a6c3270b8702794
dilli <dillihangrae@gmail.com>
2024-04-09 14:26:44 +0545

    dady95 movie recommender from tensorflow, architecture

--- ITSNP100DaysOfCoding/Month4/Day94/day94.py
+++ ITSNP100DaysOfCoding/Month4/Day94/day94.py
@@ -50,7 +50,7 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 
 
 # Finally let's look at what some of these images look like
 plt.figure()
-plt.imshow(train_images[1])
+plt.imshow(train_images[100])
 plt.colorbar()
 plt.grid(False)
 plt.show()
@@ -76,7 +76,7 @@ print(test_images)
 # (one that passes values from left tp right), we'll break down each layer and it's architecture below.
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape = (100, 100)), # input layer 1
+    keras.layers.Flatten(input_shape = (28, 28)), # input layer 1
     keras.layers.Dense(128, activation = 'relu'), # hidden layer 2
     keras.layers.Dense(10, activation ='softmax') # output layer 3
 ])
@@ -116,6 +116,59 @@ for i in range(num_images_to_show):
 
 plt.tight_layout()  # Adjust layout to prevent overlap
 plt.show()
+# Compile the Model
+# The last step in building the model is to define the loss function, optimizer and metrics we would like to track. I won't go into detail about why we chose each of these right now.
+model.compile(
+    optimizer = 'adam',
+    loss='sparse_categorical_crossentropy',
+    metrics = ['accuracy']
+)
+# Training the Model: Now it's finally time to train the model. Since we've already done all of the work on our data this step is an easy as calling a single method.
+model.fit(train_images, train_labels, epochs=10)
+# Evaluating the Model
+# Noe it's time to test/evaluate the model. We can do this quite using another built-in method from keras,
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 1)
+print('Text Accuracy : ', test_acc)
 
+predictions = model.predict(test_images)
+test_images.shape
+# This method returns to us an array of predictions for each image we passed it. Let's have a look at the predictions for image1
+print(predictions[0])
+# if we want to get the value with the highest score we can use a useful function from
+# numpy called argxmax() this simply returns the index of the maximum value from a numpy array.
+np.argmax(predictions[0])
 
+# And we can check if this is correct by looking at the value of the corresponding test label
+test_labels[0]
 
+# Verifying Predictions
+# I've written a small function here to help us verify predictions with some simple visuals.
+COLOR = 'white'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR 
+def predict(model, image, correct_label):
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    prediction = model.predict(np.array([image]))
+    predicted_class = class_names[np.argmax(prediction)]
+    show_image(image, class_names[correct_label], predicted_class)
+def show_image(img, label, guess):
+    plt.figure()
+    plt.imshow(img, cmap=plt.cm.binary)
+    plt.title("Expected: " + label)
+    plt.xlabel("Guess: " + guess)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+def get_number():
+      while True:
+        num = input("Pick a number")
+        if num.isdigit():
+            num = int(num)
+            if 0  <=  num <= 1000:
+                return int(num)
+            else:
+                print("Try again.........")
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
